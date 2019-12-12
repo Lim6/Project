f6d75c3d016d3c6f30ed526e7aa0e20bd71dea8b
Ming Long Li <lim6@wit.edu>
2019-12-12 13:56:54 -0500

    Final commit

--- Client.py
+++ Client.py
@@ -1,24 +1,27 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 import tkinter
+#Handles the socket connection
+BUFSIZ = 1024
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(("localhost", 1234))
 
-
-def receive():
-    """Handles receiving of messages."""
+def receive(): # Handles receiving the messages
+    
     while True:
         try:
             msg = client_socket.recv(BUFSIZ).decode("utf8")
             msg_list.insert(tkinter.END, msg)
-        except OSError:  # Possibly client has left the chat.
+        except OSError:  
             break
 
 
-def send(event=None):  # event is passed by binders.
-    """Handles sending of messages."""
+def send(event=None):  # Handles sending the messages.
+    
     msg = my_msg.get()
     my_msg.set("")  # Clears input field.
     client_socket.send(bytes(msg, "utf8"))
-    if msg == "{quit}":
+    if msg == "/quit":
         client_socket.close()
         top.quit()
     elif msg == "/shrug":
@@ -36,45 +39,41 @@ def send(event=None):  # event is passed by binders.
         msg = "ლ(ಠ_ಠლ)"
         client_socket.send(bytes(msg, "utf8"))
 
-def on_closing(event=None):
-    """This function is to be called when the window is closed."""
-    my_msg.set("{quit}")
+def on_closing(event=None): # This is called when the window is closed.
+   
+    my_msg.set("/quit")
     send()
-
+# This creates the GUI
 top = tkinter.Tk()
-top.title("Chatter")
+top.title("WebChat")
 
 messages_frame = tkinter.Frame(top)
-my_msg = tkinter.StringVar()  # For the messages to be sent.
+my_msg = tkinter.StringVar()  
 my_msg.set("")
-scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
-# Following will contain the messages.
-msg_list = tkinter.Listbox(messages_frame, height=20, width=65, yscrollcommand=scrollbar.set)
+scrollbar = tkinter.Scrollbar(messages_frame)  
+# This sets the size of the GUI
+msg_list = tkinter.Listbox(messages_frame, height=40, width=130, yscrollcommand=scrollbar.set)
 scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
 msg_list.pack()
 messages_frame.pack()
-
+# This creates the message bar and also the send and quit button
 entry_field = tkinter.Entry(top, textvariable=my_msg)
 entry_field.bind("<Return>", send)
 entry_field.pack()
 send_button = tkinter.Button(top, text="Send", command=send)
 send_button.pack()
 quit_button = tkinter.Button(top, text = "Quit", command = on_closing)
-quit_button.place(x=225,y=343)
+quit_button.pack()
 
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
 
-#----Now comes the sockets part----
 
 
-BUFSIZ = 1024
 
 
-client_socket = socket(AF_INET, SOCK_STREAM)
-client_socket.connect(("localhost", 1234))
-
+# Handles receiving the server and also starts the GUI
 receive_thread = Thread(target=receive)
 receive_thread.start()
-tkinter.mainloop()  # Starts GUI execution.
+tkinter.mainloop()  
--- Server.py
+++ Server.py
@@ -1,46 +1,6 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
-
-
-def accept_incoming_connections():
-    """Sets up handling for incoming clients."""
-    while True:
-        client, client_address = SERVER.accept()
-        print("%s:%s has connected." % client_address)
-        client.send(bytes("Welcome to the chatroom! Now type your name and press enter!", "utf8"))
-        addresses[client] = client_address
-        Thread(target=handle_client, args=(client,)).start()
-
-
-def handle_client(client):  # Takes client socket as argument.
-    """Handles a single client connection."""
-
-    name = client.recv(BUFSIZ).decode("utf8")
-    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
-    client.send(bytes(welcome, "utf8"))
-    msg = "%s has joined the chat!" % name
-    broadcast(bytes(msg, "utf8"))
-    clients[client] = name
-    while True:
-        msg = client.recv(BUFSIZ)
-        if msg != bytes("{quit}", "utf8"):
-            broadcast(msg, name+": ")
-       
-        else:
-            client.send(bytes("{quit}", "utf8"))
-            client.close()
-            del clients[client]
-            broadcast(bytes("%s has left the chat." % name, "utf8"))
-            break
-
-
-def broadcast(msg, prefix=""):  # prefix is for name identification.
-    """Broadcasts a message to all the clients."""
-
-    for sock in clients:
-        sock.send(bytes(prefix, "utf8")+msg)
-
-        
+# This creates the server allowing other clients to join 
 clients = {}
 addresses = {}
 
@@ -51,6 +11,48 @@ ADDR = (HOST, PORT)
 
 SERVER = socket(AF_INET, SOCK_STREAM)
 SERVER.bind(ADDR)
+
+
+def accept_incoming_connections(): # This handles sending the welcoming message to incoming connections
+    
+    while True:
+        client, client_address = SERVER.accept()
+        print("%s:%s has connected." % client_address)
+        client.send(bytes("Welcome to WebChat! Please type your name and press enter!", "utf8"))
+        addresses[client] = client_address
+        Thread(target=handle_client, args=(client,)).start()
+
+
+def handle_client(client): # This handles individual clients 
+    
+
+    name = client.recv(BUFSIZ).decode("utf8")
+    welcome = 'Welcome To WebChat %s! To leave the chatroom, type /quit.' % name
+    client.send(bytes(welcome, "utf8"))
+    msg = "%s has joined the chat!" % name
+    broadcast(bytes(msg, "utf8"))
+    clients[client] = name
+    while True:
+        msg = client.recv(BUFSIZ)
+        # This tells other clients that one of the clients have left the chat.
+        if msg != bytes("/quit", "utf8"): 
+            broadcast(msg, name+": ")
+       
+        else:
+            client.send(bytes("/quit", "utf8"))
+            client.close()
+            del clients[client]
+            broadcast(bytes("%s has left the chat." % name, "utf8"))
+            break
+
+
+def broadcast(msg, prefix=""):  # This sends each clients messages to other clients
+    
+
+    for server_socket in clients:
+        server_socket.send(bytes(prefix, "utf8")+msg)
+
+        
 
 if __name__ == "__main__":
     SERVER.listen(5)
